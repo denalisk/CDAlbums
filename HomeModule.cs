11963fe47ebd33520a08d65d9db305b76dbd3afc
Marc Larkin and Sam Kirsch <marc+sam@>
2017-02-09 13:08:04 -0800

    initialize the base level files and file connections and add the base level names

---
+++ HomeModule.cs
@@ -0,0 +1,14 @@
+using Nancy;
+using System.Collections.Generic;
+using Album.Objects;
+
+namespace Library
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            // PUT ROUTES HERE
+        }
+    }
+}
